65a294e7a81cbb5c7e7f41518db67d5cc8c91c59
Francisco Calderon <79600062+FranCalderon67@users.noreply.github.com>
2023-02-03 21:45:09 -0300

    Setting endpoints (#3)

--- chatapp/src/components/MessageContainer.jsx
+++ chatapp/src/components/MessageContainer.jsx
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from "react";
 import { Box, Typography } from "@mui/material";
 import axios from "axios"
 import io from "socket.io-client"
-const socket = io.connect("http://2530956b399a:3000")
+const socket = io.connect("https://super-chat-2w3x.onrender.com")
 export const MessageContainer = () => {
     const [message, setMessage] = useState()
 
     const getMessage = async () => {
         try {
-            const getMessage = axios.get('http://2530956b399a:3000/messages')
+            const getMessage = axios.get('https://super-chat-2w3x.onrender.com/messages')
             const result = await getMessage
             const allMessages = result.data.map((m) => { return <p key={m._id}> {m.message}  </p> })
             setMessage(allMessages)
@@ -19,10 +19,6 @@ export const MessageContainer = () => {
 
     useEffect(() => {
         getMessage()
-    }, [])
-
-
-    useEffect(() => {
         socket.on("messages", (data) => {
             getMessage(data)
         })
