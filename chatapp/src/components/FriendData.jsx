918eef713aa9e70f33c9e954bdb23ef7ee1bb2cf
Francisco Calderon <79600062+FranCalderon67@users.noreply.github.com>
2023-04-16 22:21:31 -0300

    setting user layout (#9)

---
+++ chatapp/src/components/FriendData.jsx
@@ -0,0 +1,23 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+
+export const FriendData = () => {
+    return (
+        <>
+            <Box
+                width="100%"
+                height="64px"
+
+                sx={{
+                    backgroundColor: "red"
+                }}
+            >
+                Datos de amiguito
+
+            </Box>
+
+        </>
+
+    )
+}
